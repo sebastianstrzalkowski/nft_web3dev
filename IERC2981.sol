00cd2d0a85e7164a115587957327fe0c8f9528d5
sebastianstrzalkowski <33623243+sebastianstrzalkowski@users.noreply.github.com>
2022-08-11 22:53:52 +0200

    Update IERC2981.sol

--- IERC2981.sol
+++ IERC2981.sol
@@ -10,9 +10,9 @@ interface IERC2981 is IERC165 {
     /// ERC165 bytes to add to interface array - set in parent contract
     /// implementing this standard
     ///
-    /// bytes4(keccak256("royaltyInfo(uint256,uint256)")) == 0x2a55205a
-    /// bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
-    /// _registerInterface(_INTERFACE_ID_ERC2981);
+    bytes4(keccak256("royaltyInfo(uint256,uint256)")) == 0x2a55205a
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+    _registerInterface(_INTERFACE_ID_ERC2981);
 
     /// @notice Called with the sale price to determine how much royalty
     //          is owed and to whom.
